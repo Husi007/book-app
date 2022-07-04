7766a79741ff197d83e6967b9fd9eddee71301b7
Muhammad Hussnain Athar <m.hussnainathar@gmail.com>
2022-07-05 03:04:47 +0500

    Updated server port in constant!

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const SuccessMessages = {
   DATA_BASE_CONNECTION: 'Sucessfully connected to PostGreSql Database!',
-  SERVER_LISTENING: 'Listening on Port 3000!',
+  SERVER_LISTENING: `Listening on Port ${process.env.PORT}!`,
 };
